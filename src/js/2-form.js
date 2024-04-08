8d98797fa380e5858914d70ab35ec905a1b6f03d
Anton1824 <152066703+Anton1824@users.noreply.github.com>
2024-04-08 22:18:27 +0100

    update hw

---
+++ src/js/2-form.js
@@ -0,0 +1,62 @@
+const form = document.querySelector('.feedback-form');
+let userData = {};
+
+function fillFeedbackFormFields() {
+  try {
+    const userDataFromLS = JSON.parse(
+      localStorage.getItem('feedback-form-state')
+    );
+
+    if (userDataFromLS === null) {
+      return;
+    }
+    userData = userDataFromLS;
+
+    for (const key in userDataFromLS) {
+      form.elements[key].value = userDataFromLS[key];
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+fillFeedbackFormFields();
+
+form.addEventListener('input', setNewData);
+form.addEventListener('submit', clickButton);
+
+function setNewData(event) {
+  const { target: feedbackFieldEl } = event;
+
+  const feedbackFieldName = feedbackFieldEl.name;
+  const feedbackFieldValue = feedbackFieldEl.value;
+
+  userData[feedbackFieldName] = feedbackFieldValue.trim();
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(userData));
+}
+
+function clickButton(event) {
+  if (
+    !form.elements.email.value.trim() &&
+    !form.elements.message.value.trim()
+  ) {
+    alert('Будь ласка, заповніть поле електронної пошти та поле коментару.');
+    return;
+  }
+  if (!form.elements.email.value.trim()) {
+    alert('Будь ласка, заповніть поле електронної пошти.');
+
+    return;
+  }
+
+  if (!form.elements.message.value.trim()) {
+    alert('Будь ласка, заповніть поле textarea.');
+
+    return;
+  }
+  alert('Дякуємо за ваш відгук!');
+  event.preventDefault();
+  console.log(userData);
+  localStorage.removeItem('feedback-form-state');
+  form.reset();
+}
